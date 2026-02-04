fd40555f8c2db99dd857fe101157b3e439992aa6
Буланов Александр <alexbulanovgoodmail@gmail.com>
2026-02-04 17:43:57 +0300

    Первоначальная фиксация

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+});
