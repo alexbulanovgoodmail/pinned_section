db25c83ff3b0c01cf0266fa9602875e203dc94d9
Буланов Александр <alexbulanovgoodmail@gmail.com>
2026-02-06 10:00:26 +0300

    Добавлена анимация вращения контейнеров при скролле

--- script.js
+++ script.js
@@ -10,4 +10,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 
   gsap.ticker.lagSmoothing(0);
+
+  const main = document.querySelector("main");
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((section, index) => {
+    const container = section.querySelector(".container");
+
+    gsap.to(container, {
+      rotation: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: "top bottom",
+        end: "top 20%",
+        scrub: true,
+      },
+    });
+
+    if (index === sections.length - 1) return;
+
+    ScrollTrigger.create({
+      trigger: section,
+      start: "bottom bottom",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false,
+    });
+  });
 });
